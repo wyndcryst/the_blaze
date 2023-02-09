03561df8fa15d9dad71cee94bbb2a4865b81e3f9
Ronaldo Diamante <wyndcryst25@gmail.com>
2023-02-09 18:33:45 +0800

    Created a database and connected the blaze project.

---
+++ admin/config/constants.php
@@ -0,0 +1,6 @@
+<?php
+define('ROOT_URL', 'http://localhost/blogApp/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'ronaldo');
+define('DB_PASS', 'ronaldo123');
+define('DB_NAME', 'blaze');
