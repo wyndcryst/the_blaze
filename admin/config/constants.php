c66e4153a3fd9b50c3b6f221433e92e1c9b16b34
Ronaldo Diamante <wyndcryst25@gmail.com>
2023-02-14 16:58:55 +0800

    Fixed config files issue on admin pages.

---
+++ admin/config/constants.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+
+define('ROOT_URL', 'http://localhost/blogApp/');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'ronaldo');
+define('DB_PASS', 'admin1234');
+define('DB_NAME', 'blaze');
